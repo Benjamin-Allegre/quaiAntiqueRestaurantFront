aee76b01da99fbfb19c33f4fea005695a88f7939
Benjamin-Allegre <allegre.benjamin.pro@gmail.com>
2026-09-14 17:04:16 +0200

    finish Connexion Inscription Mon compte Modifier password

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,11 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "Galerie", "/pages/galerie.html"),
-    new Route("/la-carte", "La carte", "/pages/la-carte.html"),
+    new Route("/la-carte", "La carte", "/pages/laCarte.html"),
+    new Route("/connexion", "Connexion", "/pages/signin.html"),
+    new Route("/inscription", "Inscritpion", "/pages/signup.html"),
+    new Route("/mon-compte", "Mon compte", "/pages/account.html"),
+    new Route("/modifier-password", "Mon compte", "/pages/editPassword.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 
