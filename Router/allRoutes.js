3dec9148c98aed58237f2de853d385966b24cfd3
Benjamin-Allegre <allegre.benjamin.pro@gmail.com>
2026-09-14 15:51:31 +0200

    finish La carte page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,9 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "Galerie", "/pages/galerie.html"),];
+    new Route("/galerie", "Galerie", "/pages/galerie.html"),
+    new Route("/la-carte", "La carte", "/pages/la-carte.html"),
+];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 
 export const websiteName = "Quai Antique";
